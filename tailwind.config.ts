6af3bfb208fe3bc3d1404374e399cab9581bb713
Robby6Strings <rausten93@gmail.com>
2024-02-18 23:59:14 +1300

    add small-scrollbars css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,7 +23,7 @@ export default {
       },
       backgroundImage: {
         "theme-gradient": "linear-gradient(153deg,#ba93ff,#9f3942)",
-        "theme-gradient-dark": "linear-gradient(153deg,#6e5699,#871e27)",
+        "theme-gradient-dark": "linear-gradient(153deg,#7552b3,#871e27)",
       },
     },
   },
