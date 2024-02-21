c6bc3c79e53f9be8ce1f903e275d3d6e2e05e776
Robby6Strings <rausten93@gmail.com>
2024-02-21 16:46:16 +1300

    add error page

---
+++ src/pages/_error/+Page.tsx
@@ -0,0 +1,19 @@
+import { Button } from "$/components/atoms/Button"
+import { usePageContext } from "$/context/pageContext"
+
+export { Page }
+function Page() {
+  const pageContext = usePageContext()
+
+  let msg: string = pageContext.is404
+    ? "We couldn't find the page you're looking for."
+    : "Something went wrong. Try again later."
+
+  return (
+    <div className="w-full h-screen flex flex-col gap-4 items-center justify-center text-center relative mt-[var(--navbar-height-negative)] px-5">
+      <h1 className="text-5xl">Oops!</h1>
+      <h1 className="text-3xl">{msg}</h1>
+      <Button onclick={() => history.back()}>Go back</Button>
+    </div>
+  )
+}
