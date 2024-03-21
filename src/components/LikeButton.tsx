c762f0a07af1a2879441895ee7ef5fb853667523
Robby6Strings <rausten93@gmail.com>
2024-03-22 02:01:00 +1300

    add LikeButton 'justliked' state to prevent animation rerunning

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "kaioken"
 import { HeartIcon } from "./icons/HeartIcon"
 import "./LikeButton.css"
 
@@ -8,9 +9,19 @@ export function LikeButton({
   liked: boolean
   toggleLiked: () => void
 }) {
+  const prevLiked = useRef(liked)
+  const [justLiked, setJustLiked] = useState(false)
+  useEffect(() => {
+    if (liked !== prevLiked.current) {
+      setJustLiked(liked)
+      prevLiked.current = liked
+    }
+  })
+
   return (
     <button
       data-liked={liked}
+      just-liked={justLiked}
       role="none"
       className="like-button flex transition transform active:scale-90 active:opacity-75"
       onclick={toggleLiked}
