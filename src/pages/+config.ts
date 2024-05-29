a7b1fe1e99d6228d81882de5984a1c2a5435a040
Robby6Strings <rausten93@gmail.com>
2024-05-30 06:56:01 +1200

    update keywords

--- src/pages/+config.ts
+++ src/pages/+config.ts
@@ -1,5 +1,14 @@
 export default {
-  keywords: ["Kaioken", "Typescript", "Javascript", "library", "frontend"],
+  keywords: [
+    "Kaioken",
+    "KaiokenJS",
+    "Kaioken JS",
+    "Typescript",
+    "Javascript",
+    "library",
+    "framework",
+    "frontend",
+  ],
   meta: {
     Layout: {
       env: { server: true, client: true },
