e98731d52188395fc46c33341da9cd6fa4352674
Robby6Strings <rausten93@gmail.com>
2024-06-01 00:12:55 +1200

    simplify getting-started -> 'git clone' codeblock

--- src/pages/docs/getting-started/GitCloneCode.tsx
+++ src/pages/docs/getting-started/GitCloneCode.tsx
@@ -6,37 +6,6 @@ const useSelectionStore = createStore("CSR", (set) => ({
   setSelected: (value: string) => set(value),
 }))
 
-function CSRBash({ dirname }: { dirname: string }) {
-  return (
-    <CodeBlock
-      lang="bash"
-      copy
-      className="rounded-b-lg rounded-tr-lg"
-      code={`mkdir ${dirname || "my-app"}
-cd ${dirname || "my-app"}
-git clone https://github.com/CrimsonChi/kaioken-csr-template.git .
-rm -rf .git
-pnpm i
-pnpm dev`}
-    />
-  )
-}
-function SSRBash({ dirname }: { dirname: string }) {
-  return (
-    <CodeBlock
-      lang="bash"
-      copy
-      className="rounded-b-lg rounded-tr-lg"
-      code={`mkdir ${dirname || "my-app"}
-cd ${dirname || "my-app"}
-git clone https://github.com/CrimsonChi/kaioken-ssr-template.git .
-rm -rf .git
-pnpm i
-pnpm dev`}
-    />
-  )
-}
-
 export function GitCloneCode() {
   const { value: selectedItem, setSelected } = useSelectionStore()
   const [ref, txt] = useModel<HTMLInputElement, string>("")
@@ -54,11 +23,17 @@ export function GitCloneCode() {
           className="px-2 my-1 text-sm w-[140px] xs:w-auto bg-dark border rounded"
         />
       </div>
-      {selectedItem === "CSR" ? (
-        <CSRBash dirname={txt} />
-      ) : (
-        <SSRBash dirname={txt} />
-      )}
+      <CodeBlock
+        lang="bash"
+        copy
+        className="rounded-b-lg rounded-tr-lg"
+        code={`mkdir ${txt || "my-app"}
+cd ${txt || "my-app"}
+git clone https://github.com/CrimsonChi/kaioken-${selectedItem.toLowerCase()}-template.git .
+rm -rf .git
+pnpm i
+pnpm dev`}
+      />
     </div>
   )
 }
--- src/renderer/+onRenderClient.tsx
+++ src/renderer/+onRenderClient.tsx
@@ -10,14 +10,13 @@ let appInstance: AppContext | undefined
 export const onRenderClient: OnRenderClientAsync = async (pageContext) => {
   const container = document.getElementById("page-root")!
 
-  const isInitialLoad = pageContext.isHydration
-
-  if (isInitialLoad || !appInstance) {
+  if (pageContext.isHydration || !appInstance) {
     appInstance = await hydrate(App, container, { pageContext })
     return
   }
 
   document.title = getTitle(pageContext)
-  appInstance.rootNode!.child!.props.pageContext = pageContext
-  appInstance.requestUpdate(appInstance.rootNode!)
+  const appNode = appInstance.rootNode!.child!
+  appNode!.props.pageContext = pageContext
+  appInstance.requestUpdate(appNode)
 }
