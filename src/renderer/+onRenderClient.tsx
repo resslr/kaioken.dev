cbdcbcad1bc333599e70448c44252a513dedf791
Robby6Strings <rausten93@gmail.com>
2024-06-01 01:06:20 +1200

    update kaioken version

--- src/renderer/+onRenderClient.tsx
+++ src/renderer/+onRenderClient.tsx
@@ -1,22 +1,20 @@
 // https://vike.dev/onRenderClient
-import type { OnRenderClientAsync } from "vike/types"
+import type { OnRenderClientAsync, PageContextClient } from "vike/types"
 import { hydrate } from "kaioken/ssr"
 import type { AppContext } from "kaioken"
 import { getTitle } from "./utils"
 import { App } from "./App"
 
-let appInstance: AppContext | undefined
+let appContext: AppContext<{ pageContext: PageContextClient }> | undefined
 
 export const onRenderClient: OnRenderClientAsync = async (pageContext) => {
   const container = document.getElementById("page-root")!
 
-  if (pageContext.isHydration || !appInstance) {
-    appInstance = await hydrate(App, container, { pageContext })
+  if (pageContext.isHydration || !appContext) {
+    appContext = await hydrate(App, container, { pageContext })
     return
   }
 
   document.title = getTitle(pageContext)
-  const appNode = appInstance.rootNode!.child!
-  appNode!.props.pageContext = pageContext
-  appInstance.requestUpdate(appNode)
+  appContext.setProps(() => ({ pageContext }))
 }
