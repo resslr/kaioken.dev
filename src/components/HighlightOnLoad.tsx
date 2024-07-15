f66b220c8bca1a0d8b0d0c48de57f58979d43e35
Robby6Strings <rausten93@gmail.com>
2024-07-16 00:50:37 +1200

    add doc section highlight on hash change / page load

---
+++ src/components/HighlightOnLoad.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useRef } from "kaioken"
+
+export function HighlightOnLoad({
+  children,
+  hash,
+}: {
+  children: JSX.Children
+  hash: string
+}) {
+  const ref = useRef<HTMLDivElement | null>(null)
+  const timeoutRef = useRef(-1)
+  useEffect(() => {
+    if (!ref.current) return
+    if (window.location.hash === hash) {
+      ref.current.classList.add("highlight")
+      if (timeoutRef.current !== -1) {
+        window.clearTimeout(timeoutRef.current)
+      }
+      timeoutRef.current = window.setTimeout(
+        () => ref.current?.classList.remove("highlight"),
+        1500
+      )
+    }
+  }, [globalThis.window?.location.hash])
+  return (
+    <div className="docs-section" ref={ref}>
+      {children}
+    </div>
+  )
+}
