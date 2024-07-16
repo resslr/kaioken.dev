101c010e4504d8769163458032baf56f53d1761d
Robby6Strings <rausten93@gmail.com>
2024-07-17 06:45:11 +1200

    tweak landing page code demo styles

--- src/components/demos/CodeDemo.tsx
+++ src/components/demos/CodeDemo.tsx
@@ -12,7 +12,7 @@ export function CodeDemo({
   //bg-[#1c1c1c]
   return (
     <div className="grid md:grid-cols-5">
-      <div className="flex flex-col md:col-span-3 md:rounded-2xl bg-[#222] text-light overflow-hidden z-10 shadow-[#0006] shadow-lg">
+      <div className="flex flex-col md:col-span-3 md:rounded-2xl bg-[#1f1e1c] text-light overflow-hidden z-10 shadow-[#0006] shadow-lg">
         <span className="text-sm px-4 py-2 text-stone-200 opacity-80">
           {filename}
         </span>
