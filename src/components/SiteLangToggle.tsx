1443f70feee4f81dcb89fec8174cedead4dece23
Robby6Strings <rausten93@gmail.com>
2024-08-26 04:14:12 +1200

    tinker with SiteLangToggle

--- src/components/SiteLangToggle.tsx
+++ src/components/SiteLangToggle.tsx
@@ -1,19 +1,28 @@
 import { siteCodeLang } from "$/state/langToggle"
-import { useCallback } from "kaioken"
+import { useCallback, useRef } from "kaioken"
 
 export function SiteLangToggle() {
   const handleClick = useCallback(() => {
     siteCodeLang.value = siteCodeLang.value === "js" ? "ts" : "js"
   }, [])
+  const btnRef = useRef<HTMLButtonElement | null>(null)
+  const tx =
+    siteCodeLang.value === "ts"
+      ? btnRef.current?.getBoundingClientRect().width
+      : 0
   return (
     <button
+      ref={btnRef}
       className="relative flex items-end justify-end text-primary border-2 border-primary rounded p-1 w-7 h-7 text-xs leading-none font-extrabold overflow-hidden"
       onclick={handleClick}
     >
       <div
-        className={`transition absolute right-1 ${siteCodeLang.value === "ts" ? "translate-x-[1.325rem]" : ""}`}
+        style={{
+          transform: `translateX(${tx}px)`,
+        }}
+        className={`transition absolute right-1 w-[200%] text-right`}
       >
-        <span style="color: crimson; margin-right: 0.5rem">TS</span>
+        <span style="color: crimson; margin-right: 0.9375rem">TS</span>
         <span style="color: crimson">JS</span>
       </div>
     </button>
