fe9190d8195769ec7600a55a518ff191741a107a
Robby6Strings <rausten93@gmail.com>
2024-09-22 14:22:44 +1200

    improve tutorial

--- src/pages/tutorial/+Layout.tsx
+++ src/pages/tutorial/+Layout.tsx
@@ -4,13 +4,13 @@ import { TutorialSandbox } from "./TutorialSandbox"
 
 export function Layout({ children }: { children: JSX.Children }) {
   return (
-    <Container className="flex gap-8 mt-[var(--navbar-height)] min-h-[calc(100dvh+var(--navbar-height-negative))]">
-      <TutorialStepProvider>
+    <TutorialStepProvider>
+      <Container className="flex gap-8 mt-[var(--navbar-height)] min-h-[calc(100dvh+var(--navbar-height-negative))]">
         <div className="prose prose-invert flex-grow">{children}</div>
         <div className="flex-grow w-full">
           <TutorialSandbox />
         </div>
-      </TutorialStepProvider>
-    </Container>
+      </Container>
+    </TutorialStepProvider>
   )
 }
