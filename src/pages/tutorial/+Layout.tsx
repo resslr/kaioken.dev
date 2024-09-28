bca08f50757cc564c575e02de7be4a0ce362340a
Robby6Strings <rausten93@gmail.com>
2024-09-28 20:41:25 +1200

    improve tutorial - add subnav, fix layout

--- src/pages/tutorial/+Layout.tsx
+++ src/pages/tutorial/+Layout.tsx
@@ -4,10 +4,27 @@ import { TutorialSandbox } from "./TutorialSandbox"
 export function Layout({ children }: { children: JSX.Children }) {
   return (
     <TutorialStepProvider>
-      <div className="flex mt-[var(--navbar-height)] min-h-[calc(100dvh+var(--navbar-height-negative))]">
-        <div className="prose prose-invert w-full p-8">{children}</div>
-        <div className="flex-grow w-full p-2">
-          <TutorialSandbox />
+      <div className="flex flex-col h-full mt-[var(--navbar-height)]">
+        <div className="flex flex-grow h-[calc(100dvh+var(--navbar-height-negative))]">
+          <div className="bg-[#111] prose prose-invert w-full p-8 overflow-y-auto max-h-[calc(100dvh+var(--navbar-height-negative))]">
+            <nav className="not-prose sticky w-full top-0">
+              <ul className="flex gap-2">
+                <li>
+                  <a href="#step-1">Step 1</a>
+                </li>
+                <li>
+                  <a href="#step-2">Step 2</a>
+                </li>
+                <li>
+                  <a href="#step-3">Step 3</a>
+                </li>
+              </ul>
+            </nav>
+            {children}
+          </div>
+          <div className="flex-grow w-full">
+            <TutorialSandbox />
+          </div>
         </div>
       </div>
     </TutorialStepProvider>
