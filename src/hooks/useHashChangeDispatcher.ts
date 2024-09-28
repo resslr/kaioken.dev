c02455526cc36faa965a12f57d41e7b32a302cba
Robby6Strings <rausten93@gmail.com>
2024-09-29 06:02:49 +1300

    simplify hashChangeDispatcher

--- src/hooks/useHashChangeDispatcher.ts
+++ src/hooks/useHashChangeDispatcher.ts
@@ -1,3 +1,4 @@
+import { usePageContext } from "$/context/pageContext"
 import { customEvents } from "$/custom-events"
 import { useEffect, useRef, useState } from "kaioken"
 
@@ -37,84 +38,167 @@ import { useEffect, useRef, useState } from "kaioken"
 //   return currentSection
 // }
 
+// export const useHashChangeDispatcher = (sections: string[]) => {
+//   const currentSection = useRef<string>("")
+//   const hasScrolled = useRef(false)
+
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       hasScrolled.current = true
+//       if (globalThis.window.scrollY < 50) {
+//         dispatchHashChange("", currentSection.current)
+//         currentSection.current = ""
+//       } else if (currentSection.current === "" && window.location.hash === "") {
+//         currentSection.current = sections[0]
+//         dispatchHashChange(currentSection.current, "")
+//       }
+//     }
+//     window.addEventListener("scroll", handleScroll)
+//     return () => window.removeEventListener("scroll", handleScroll)
+//   }, [sections])
+
+//   useEffect(() => {
+//     const sectionElements = sections.map((id) => document.getElementById(id))
+
+//     const observer = new IntersectionObserver(
+//       (entries) => {
+//         if (!hasScrolled.current) {
+//           const section = document.getElementById(
+//             window.location.hash.split("#")[1]
+//           )
+//           section?.scrollIntoView({
+//             behavior: "smooth",
+//             block: "start",
+//           })
+//           return
+//         }
+//         if (globalThis.window.scrollY < 50) {
+//           if (document.body.clientHeight > window.innerHeight) {
+//             dispatchHashChange("", currentSection.current)
+//           }
+//           return
+//         }
+
+//         const maxVisible = entries.reduce<IntersectionObserverEntry | null>(
+//           (max, entry) =>
+//             entry.intersectionRatio > (max?.intersectionRatio || 0)
+//               ? entry
+//               : max,
+//           null
+//         )
+//         if (!maxVisible) return
+//         console.log("maxVisible", maxVisible)
+
+//         for (let i = 0; i < entries.length; i++) {
+//           const entry = entries[i]
+//           if (!entry.isIntersecting && entries.length === 1) {
+//             // left a short section?
+//             const prev = currentSection.current
+//             const sectionIdx = sections.indexOf(entry.target.id)
+//             if (sectionIdx === 0) {
+//               currentSection.current = sections[1]
+//             } else if (sectionIdx === sections.length - 1) {
+//               currentSection.current = sections[sectionIdx - 1]
+//             }
+
+//             if (prev !== currentSection.current) {
+//               dispatchHashChange(currentSection.current, prev)
+//             }
+//             break
+//           }
+//           if (entry.isIntersecting && entry.intersectionRatio > 0) {
+//             dispatchHashChange(entry.target.id, currentSection.current)
+//             currentSection.current = entry.target.id
+//             break
+//           }
+//         }
+//       },
+//       {
+//         threshold: 0,
+//         rootMargin: "-100px",
+//       }
+//     )
+
+//     sectionElements.forEach((section) => {
+//       if (section) {
+//         observer.observe(section)
+//       }
+//     })
+
+//     return () => {
+//       observer.disconnect()
+//     }
+//   }, [sections, currentSection])
+// }
+
 export const useHashChangeDispatcher = (sections: string[]) => {
-  const currentSection = useRef<string>("")
-
+  const [currentSection, setCurrentSection] = useState<string>("")
+  const { urlPathname } = usePageContext()
+  const hasScrolled = useRef(false)
   useEffect(() => {
-    const handleScroll = () => {
-      if (globalThis.window.scrollY < 50) {
-        dispatchHashChange("", currentSection.current)
-        currentSection.current = ""
-      } else if (currentSection.current === "") {
-        currentSection.current = sections[0]
-        dispatchHashChange(currentSection.current, "")
-      }
+    hasScrolled.current = false
+  }, [urlPathname])
+
+  const handleScroll = () => {
+    if (!hasScrolled.current) {
+      hasScrolled.current = true
+      return
     }
-    window.addEventListener("scroll", handleScroll)
-    return () => window.removeEventListener("scroll", handleScroll)
-  }, [sections])
+    if (globalThis.window.scrollY < 50) {
+      setCurrentSection("")
+      return dispatchHashChange("")
+    }
+    let closestSection = ""
+    let closestDistance = Infinity
 
-  useEffect(() => {
-    const sectionElements = sections.map((id) => document.getElementById(id))
-
-    const observer = new IntersectionObserver(
-      (entries) => {
-        if (globalThis.window.scrollY < 50)
-          return dispatchHashChange("", currentSection.current)
-        for (let i = 0; i < entries.length; i++) {
-          const entry = entries[i]
-          if (!entry.isIntersecting && entries.length === 1) {
-            // left a short section?
-            const prev = currentSection.current
-            const sectionIdx = sections.indexOf(entry.target.id)
-            if (sectionIdx === 0) {
-              currentSection.current = sections[1]
-            } else if (sectionIdx === sections.length - 1) {
-              currentSection.current = sections[sectionIdx - 1]
-            }
-
-            if (prev !== currentSection.current) {
-              dispatchHashChange(currentSection.current, prev)
-            }
-            break
-          }
-          if (entry.isIntersecting && entry.intersectionRatio > 0) {
-            dispatchHashChange(entry.target.id, currentSection.current)
-            currentSection.current = entry.target.id
-            break
-          }
-        }
-      },
-      {
-        threshold: 0,
-        rootMargin: "-100px",
-      }
-    )
-
-    sectionElements.forEach((section) => {
+    sections.forEach((id) => {
+      const section = document.getElementById(id)
       if (section) {
-        observer.observe(section)
+        // Get the distance from the top of the viewport
+        const rect = section.getBoundingClientRect()
+        const rectTop = rect.top
+        const rectBottom = rect.bottom
+        if (rectBottom > window.innerHeight + window.scrollY + 500) {
+          return
+        }
+
+        // If the section is close to or above the top (but not too far below)
+        if (rectTop >= -150 && rectTop < closestDistance) {
+          closestSection = id
+          closestDistance = rectTop
+        }
       }
     })
 
+    const newHash = `${closestSection}`
+    if (closestSection && currentSection !== newHash) {
+      setCurrentSection(newHash)
+      dispatchHashChange(newHash)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll)
+
+    // Run on initial mount to handle the case when the page is loaded scrolled partway down
+    handleScroll()
+
     return () => {
-      observer.disconnect()
+      window.removeEventListener("scroll", handleScroll)
     }
   }, [sections, currentSection])
+
+  return currentSection
 }
 
-function dispatchHashChange(newId: string, prevId: string) {
-  const hash = newId ? `#${newId}` : ""
-
-  // Only update if the section is not already set as the current section
-  if (prevId !== hash || window.location.hash !== hash) {
-    //window.history.replaceState(null, "", hash)
-    // Update the URL hash without reloading the page
-    if (hash !== "") {
-      window.history.pushState(null, "", hash)
-    } else {
-      window.history.pushState(null, "", window.location.href.split("#")[0])
-    }
-    window.dispatchEvent(new customEvents.scrollHashChangeEvent())
+function dispatchHashChange(newHash: string) {
+  const hash = newHash ? `#${newHash}` : ""
+  if (hash !== "") {
+    window.history.pushState(null, "", hash)
+  } else {
+    window.history.pushState(null, "", window.location.href.split("#")[0])
   }
+
+  // Dispatch a hashchange event
+  window.dispatchEvent(new customEvents.scrollHashChangeEvent())
 }
