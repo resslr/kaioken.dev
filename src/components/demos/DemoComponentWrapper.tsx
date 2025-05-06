b519b70135cb79a774b33ef79c568320a8b955d5
LankyMoose <rausten93@gmail.com>
2025-05-06 20:03:42 +1200

    improve 'theme-gradient'

--- src/components/demos/DemoComponentWrapper.tsx
+++ src/components/demos/DemoComponentWrapper.tsx
@@ -1,13 +1,29 @@
-import { ElementProps } from "kaioken"
+import { ElementProps, unwrap } from "kaioken"
+
+type ClassnameAttr = ElementProps<"div">["className"]
+
+function classNameIncludes(className: ClassnameAttr, value: string): boolean {
+  const v = unwrap(className)
+  if (typeof v === "string") {
+    return (v.indexOf("p-") ?? -1) > -1
+  } else if (Array.isArray(v)) {
+    return v.flat().some((item) => !!item && classNameIncludes(item, value))
+  }
+  return false
+}
 
 export function DemoComponentWrapper({
   children,
   className,
 }: ElementProps<"div">) {
-  const classIncludesPadding = (className?.indexOf("p-") ?? -1) > -1
+  const classIncludesPadding = classNameIncludes(className, "p-")
   return (
     <div
-      className={`${classIncludesPadding ? "" : "p-4"} bg-[#1c1a1a] shadow-[#0005] shadow-lg grow rounded-lg ${className || ""}`}
+      className={[
+        classIncludesPadding ? "" : "p-4",
+        `bg-[#1c1a1a] shadow-[#0005] shadow-lg grow rounded-lg`,
+        unwrap(className),
+      ]}
     >
       {children}
     </div>
